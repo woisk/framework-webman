1505b2ad889de60b837186ee2be66a1324ab691a
woisks <bolelin@126.com>
2022-04-02 20:31:41 +0800

    撤销mongodb 事务

--- Model.php
+++ Model.php
@@ -4,19 +4,8 @@ namespace Webman;
 
 use Jenssegers\Mongodb\Eloquent\Model as BaseModel;
 
-//use Webman\Mongodb\Builder;
 
 class Model extends BaseModel
 {
-//    /**
-//     * @inheritdoc
-//     ********************************************
-//     * @return Builder
-//     */
-//    protected function newBaseQueryBuilder(): Builder
-//    {
-//        $connection = $this->getConnection();
-//
-//        return new Builder($connection, $connection->getPostProcessor());
-//    }
+
 }
